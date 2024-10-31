214f67a69378e6fa8972f2e3039e88c6b366c44a
Ildefonso Valenzuela <ponchovalo@outlook.com>
2024-10-31 16:39:31 -0600

    plantilla

--- supervisor-ui/src/app/indicadores/pages/plantillas/plantillas.component.ts
+++ supervisor-ui/src/app/indicadores/pages/plantillas/plantillas.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class PlantillasComponent {
 
+  partidas: string[] = ['P-01', 'P-02', 'P-03',
+                        'P-04', 'P-05', 'P-06',
+                        'P-07', 'P-08', 'P-09',
+                        'P-10', 'P-11', 'P-12'];
+
+
+  dispositivos: string[] = ['SWITCH', 'SERVIDOR', 'TELEFONO']
+
+
+
 }
